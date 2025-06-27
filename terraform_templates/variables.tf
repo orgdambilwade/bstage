dcbb211c5412b6ba4e4d15dac4a846f77df49603
dambilwade <65251622+dambilwade@users.noreply.github.com>
2025-06-27 17:04:44 -0400

    75

---
+++ terraform_templates/variables.tf
@@ -0,0 +1,19 @@
+variable "service_account" {
+    description = "Service account to be used"
+    type        = string
+}
+
+variable "project_id" {
+    description = "Project ID to be used"
+    type        = string
+}
+
+variable "region" {
+    description = "region to be used"
+    type        = string
+}
+
+variable "zone" {
+    description = "zone to be used"
+    type        = string
+}
