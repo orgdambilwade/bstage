d15e9721c87b606ff5d7e3da2e36382d18dbc7a1
dambilwade <65251622+dambilwade@users.noreply.github.com>
2025-06-27 09:44:24 -0400

    60

--- terraform_templates/main.tf
+++ terraform_templates/main.tf
@@ -15,6 +15,41 @@ terraform {
   }
 }
 
+// start
+
+provider "google" {
+}
+
+data "google_client_config" "default" {
+  provider = google
+}
+
+data "google_service_account_access_token" "default" {
+  provider               = google
+  target_service_account = "service-account-1@eternal-argon-461501-a8.iam.gserviceaccount.com"
+  scopes                 = ["userinfo-email", "cloud-platform"]
+  lifetime               = "300s"
+}
+
+/*
+provider "google" {
+  alias        = "impersonated"
+  access_token = data.google_service_account_access_token.default.access_token
+}*/
+
+data "google_client_openid_userinfo" "me" {
+  provider = google.impersonated
+}
+
+output "target-email" {
+  value = data.google_client_openid_userinfo.me.email
+}
+
+
+
+
+//end
+
 
 provider "google" {
   project = "eternal-argon-461501-a8"
